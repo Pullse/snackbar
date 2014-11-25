9c50ba14f1aaa8c420eb58e2c0e3f4eb3ab0efb5
Ruben Zorgman <ruben.zorgman@42.nl>
2014-11-25 09:06:23 +0100

    Changed README.md, removed some elements from the POM, added first testclass

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Snackbar
 ========
 
 Snackbar application for AngularJS practice project.
+
+Get Started
+-----------
+
+* mvn clean test-compile tomcat7:run
---
+++ src/test/java/nl/mad/snackbar/SnackControllerTest.java
@@ -0,0 +1,9 @@
+package nl.mad.snackbar;
+
+public class SnackControllerTest {
+
+    public SnackControllerTest() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
